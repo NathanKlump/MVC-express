78e6edbfa866fe314b92e7347e64bcddf7b9a3ac
Nathaniel Klump <nathanieljklump@gmail.com>
2023-10-26 14:59:32 -0400

    full post route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Nav from './components/Nav'
+import IncidentForm from './components/IncidentForm'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <IncidentForm/>
     </div>
   );
 }
