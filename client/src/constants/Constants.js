66ed788752fa333de9d2e8ad64ad437fb507e62e
xiachen97 <122903592+xiachen97@users.noreply.github.com>
2023-11-14 19:55:03 -0500

    Update Constants.js

--- client/src/constants/Constants.js
+++ client/src/constants/Constants.js
@@ -1,22 +1,31 @@
+//consistent with 2022-2023 old states databse, and add some new state
 export const states = [
+  "Ahemdabad",
   "Andhra Pradesh",
   "Arunachal Pradesh",
   "Assam",
   "Bihar",
   "Chhattisgarh",
+  "Delhi",
   "Goa",
   "Gujarat",
   "Haryana",
   "Himachal Pradesh",
+  "Hyderabad",
+  "Jammu",
+  "Jammu and Kashmir",
   "Jharkhand",
   "Karnataka",
   "Kerala",
-  "Madhya Pradesh",
+  "Lakshwadeep",
+  "Madhya Pradesh",
   "Maharashtra",
   "Manipur",
   "Meghalaya",
   "Mizoram",
+  "Mumbai",
   "Nagaland",
+  "New Delhi",
   "Odisha",
   "Punjab",
   "Rajasthan",
@@ -24,7 +33,7 @@ export const states = [
   "Tamil Nadu",
   "Telangana",
   "Tripura",
-  "Uttar Pradesh",
+  "Uttar Pradesh",
   "Uttarakhand",
   "West Bengal",
   "Delhi",
@@ -34,14 +43,15 @@ export const states = [
   "Puducherry",
 ];
 
-// export const categories = [
-//   "Caste Discrimination",
-//   "Caste-based violence",
-//   "Hate/Hate Crimes/ Hate Speech by the Majoritarian Community",
-//   "Vigilante violence",
-//   "Sexual violence",
-//   "State/ Police/ Judicial Violence",
-// ];
+export const categories = [
+  "Caste Discrimination",
+  "Caste-based violence",
+  "Hate/Hate Crimes/ Hate Speech by the Majoritarian Community",
+  "Vigilante violence",
+  "Sexual violence",
+  "State / Police/ Judicial Violence" /* Please do not change any spaces for value, it needs to be consistent with the database */,
+  "Other violence",
+];
 
 export const category_Caste_Discrimination = [
   "caste in schools",
@@ -109,85 +119,3 @@ export const category_State = [
   "Curbing of dissent/demonstrations",
   "Arbitrary arrest",
 ];
-
-
-/*
-export const categories = [
-  "Select a category",
-  "Caste Discrimination",
-  "Caste-based violence",
-  "Hate/Hate Crimes/ Hate Speech by the Majoritarian Community",
-  "Vigilante violence",
-  "Sexual violence",
-  "State/ Police/ Judicial Violence"
-];
-
-export const subcategories = {
-  'Caste Discrimination': [
-    'caste in schools',
-    'caste in jobs',
-    'caste in higher education',
-    'caste in renting practices',
-    'caste in wedding customs/rituals',
-    'activities promoting segregation based on religion/caste',
-    'economic boycott of religious and caste minorities',
-    'Denied entry in temples'
-  ],
-  'Caste-based violence': [
-    'Religious Discrimination',
-    'religion in schools',
-    'religion in jobs',
-    'religion in higher education',
-    'religion in house renting practices',
-    'religion based boycott in buying/selling property',
-    'religion in cases of public transport',
-    'activities promoting segregation based on religion/caste',
-    'curbing freedom to practice religion',
-    'economic boycott of religious and caste minorities',
-    'Islamophobia',
-    'Boycotts of Muslims',
-    'Restricting entry of Muslim'
-  ],
-  'Hate/Hate Crimes/ Hate Speech by the Majoritarian Community': [
-    'Violent/hate speech by a politician',
-    'Media inciting hatred',
-    'Majoritarian violence against religious and caste minorities',
-    'Arming/training of Hindu groups',
-    'Majoritarian mob violence against religious and caste minorities',
-    'Majoritarian mob violence during Hindu festivals',
-    'Demolition/vandalisation of a minority religious/cultural site',
-    'Criminalisation of prayers (namaz)',
-    'Lynching'
-  ],
-  'Vigilante violence': [
-    'moral policing',
-    'love jihad',
-    'cattle protection groups',
-    'offering prayers in open',
-    "valentine's day",
-    'Christmas celebrations in school/church',
-    'complaint against teachers/professors for remarks on Hinduism',
-    'curbing economic activities of Muslims around Hindu festivals'
-  ],
-  'Sexual violence': [
-    'caste based sexual violence',
-    'religion based sexual violence',
-    'gender-identity based sexual violence',
-    'sexual violence against minors'
-  ],
-  'State/ Police/ Judicial Violence': [
-    'Demolitions of houses, shops, schools belonging to religious and caste minorities',
-    'Demolition under the pretext of encroachment',
-    'Demolition of houses of protestors',
-    'Police violence/violence in police custody',
-    'Police violence during a protest',
-    'Arrest under anti-terror law for dissent/writing/protesting',
-    'Arrest/violence for supporting a cricket team',
-    'Misuse of law to marginalise',
-    'Arrest of journalists',
-    'Curbing of dissent/demonstrations',
-    'Arbitrary arrest'
-  ]
-};
-*/
-
